064f7b34cfd9e0ef2563df3a52a1864818d5b03d
Juan Sugg <juanpedrosugg@gmail.com>
2026-04-21 23:18:13 -0300

    refactor(tests): migrate testing suite paths to taf

--- tests/suites/contracts/taf/test_monkeypatch_governance.py
+++ tests/suites/contracts/taf/test_monkeypatch_governance.py
@@ -6,7 +6,7 @@ from tests.utils.helpers.repo import REPO_ROOT
 from tests.utils.scripts.analyze_fixtures import analyze_fixture_tree
 
 _ALLOWED_DIRECT_MONKEYPATCH_FILES = {
-    "tests/suites/contracts/testing/test_env_isolation_contract.py",
+    "tests/suites/contracts/taf/test_env_isolation_contract.py",
     "tests/suites/unit/clawops/hypermemory/test_capture.py",
     "tests/suites/unit/clawops/hypermemory/test_providers.py",
     "tests/suites/unit/clawops/test_acp_runner.py",
@@ -17,9 +17,9 @@ _ALLOWED_DIRECT_MONKEYPATCH_FILES = {
     "tests/suites/unit/clawops/test_setup_cli.py",
     "tests/suites/unit/clawops/test_strongclaw_ops.py",
     "tests/suites/unit/clawops/test_supply_chain.py",
-    "tests/suites/unit/testing/test_identity.py",
-    "tests/suites/unit/testing/test_mode_resolution.py",
-    "tests/suites/unit/testing/test_test_context.py",
+    "tests/suites/unit/taf/test_identity.py",
+    "tests/suites/unit/taf/test_mode_resolution.py",
+    "tests/suites/unit/taf/test_test_context.py",
 }
 
 
